ca53ff7b593cc265dbf0de18b7e8494d398b3431
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-09-19 02:18:02 +0700

    Add project files.

---
+++ ComboBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyWPFApp
+{
+    /// <summary>
+    /// Interaction logic for ComboBox.xaml
+    /// </summary>
+    public partial class ComboBox : Window
+    {
+        public ComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void cboColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var stackPannel = (StackPanel) cboColor.SelectedItem;
+            var label = stackPannel.Children[1] as Label;
+            string color = label.Content.ToString();
+            lbMsg.Content = "Your color is " + color;
+        }
+    }
+}
